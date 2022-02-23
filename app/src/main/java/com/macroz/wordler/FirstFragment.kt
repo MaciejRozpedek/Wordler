49ee39f98a7d00c7021c2f7ae5bff3dbf9eb906f
Maciej Rozpedek <maciek.rozpedek@gmail.com>
2022-02-23 11:38:28 +0100

    Added motivating text

--- app/src/main/java/com/macroz/wordler/FirstFragment.kt
+++ app/src/main/java/com/macroz/wordler/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
 import com.macroz.wordler.databinding.FragmentFirstBinding
 
@@ -35,6 +37,7 @@ class FirstFragment : Fragment() {
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+        view.findViewById<TextView>(R.id.motivatingText).text = "You're the best"
     }
 
     override fun onDestroyView() {
